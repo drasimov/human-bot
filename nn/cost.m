f06407514e86fa42636bbd6f5f6f3117939cecfa
drasimov <wang_aaron_@hotmail.com>
2025-04-07 13:12:02 +0800

    Removed regularization in cost

--- nn/cost.m
+++ nn/cost.m
@@ -1,10 +1,4 @@
 function c = cost(x,y,type,derivative,w)
-    global lambda;
-    global N_TRAIN;
-    sum_weight = 0;
-    for i = 1:length(w)
-        sum_weight = sum_weight + sum(w{i}.^2,"all");
-    end
     if derivative
         switch type
             case "quadratic"
@@ -19,6 +13,5 @@ function c = cost(x,y,type,derivative,w)
             case "cross-entropy"
                 c = -sum(y.*log(x) + (1-y).*log(1-x));
         end
-        c = c + lambda/(2*N_TRAIN)*sum_weight;
     end
 end
